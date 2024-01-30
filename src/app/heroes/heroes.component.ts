765948bc44a49a9f35dabbdb8d4b63100d932645
Boyu Li <lby1251853265@icloud.com>
2024-01-30 23:30:47 +0800

    feat: hero could display details

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,10 +1,12 @@
+import { UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, UpperCasePipe],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.sass'
 })
