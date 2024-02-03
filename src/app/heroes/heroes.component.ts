73e58b66c5f2de9b6a7c0108953bbba2e3b140c6
Boyu Li <lby1251853265@icloud.com>
2024-02-03 11:50:21 +0800

    feat: hero list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,18 +1,15 @@
-import { UpperCasePipe } from '@angular/common';
+import { NgFor, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
-import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [FormsModule, UpperCasePipe],
+  imports: [FormsModule, UpperCasePipe, NgFor],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.sass'
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES
 }
