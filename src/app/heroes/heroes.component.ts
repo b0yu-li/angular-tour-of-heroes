d08502db5cb8875a62b675f10dab955d42661572
Boyu Li <lby1251853265@icloud.com>
2024-02-04 23:57:58 +0800

    feat: select visual cue

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,4 @@
-import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
+import { NgClass, NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { HEROES } from '../mock-heroes';
@@ -7,13 +7,20 @@ import { Hero } from '../hero';
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [FormsModule, UpperCasePipe, NgFor, NgIf],
+  imports: [FormsModule, UpperCasePipe, NgFor, NgIf, NgClass],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.sass'
 })
 export class HeroesComponent {
   heroes = HEROES
   selectedHero?: Hero;
+
+  renderSelectBtn(hero: Hero): string {
+    if (this.selectedHero === hero) {
+      return "selected".toUpperCase()
+    }
+    return "select".toUpperCase()
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
--- tasking.md
+++ tasking.md
@@ -1,5 +1,6 @@
 ## 02-04
 - [x] push to GitHub repo
+- [x] card selected visual cue
 - [ ] add pre-commit hook
 
 ## 02-03
