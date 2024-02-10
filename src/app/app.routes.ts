ba76be496bb2f2451e08a7e49cb3e2a653bd6935
Boyu Li <lby1251853265@icloud.com>
2024-02-10 23:18:43 +0800

    feat: add dashbooard route config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 export const routes: Routes = [
+    { path: 'dashboard', component: DashboardComponent },
     { path: 'detail/:id', component: HeroDetailComponent },
     { path: 'heroes', component: HeroesComponent }
 ];
