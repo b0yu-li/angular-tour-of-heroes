a1e3c836b0a865356a029a61e2f492fb8199430b
Boyu Li <lby1251853265@icloud.com>
2024-02-14 15:43:25 +0800

    feat: introduce http client

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -35,4 +35,18 @@ export class HeroesComponent {
     this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
     this.router.navigate(['/', 'detail', hero.id]);
   }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
 }
