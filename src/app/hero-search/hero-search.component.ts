7fceed428eba0e78145f88869d9b8089028c3b8f
Boyu Li <lby1251853265@icloud.com>
2024-02-15 23:49:49 +0800

    feat: hero search html part

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -1,12 +1,21 @@
+import { AsyncPipe, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-hero-search',
   standalone: true,
-  imports: [],
+  imports: [NgFor, RouterModule, AsyncPipe],
   templateUrl: './hero-search.component.html',
   styleUrl: './hero-search.component.sass'
 })
 export class HeroSearchComponent {
+  heroes$!: Observable<Hero[]>;
+
+  search(arg0: string) {
+    throw new Error('Method not implemented.');
+  }
 
 }
